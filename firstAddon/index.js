a2cdcf59d4394540f767f171f5848cb295007fa3
dawahent <dawahent@gmail.com>
2018-10-15 01:27:51 -0500

    put on mask

--- firstAddon/index.js
+++ firstAddon/index.js
@@ -1,17 +1,31 @@
 'use strict';
 //executing js file in specified file directory 'fileDir'
 function execFile(fileDir){
-    return function(){
-        //following cmd will be exec in the scope of web page currently looked at
-        //NOT in scope of Scraper Go extenstion!
-        chrome.tabs.executeScript({
-            file: fileDir
-        });
-    }
+    //following cmd will be exec in the scope of web page currently looked at
+    //NOT in scope of Scraper Go extenstion!
+    chrome.tabs.executeScript({
+        file: fileDir
+    });
 }
 
 //get the buttons
 let goButton = document.getElementById("goButton");
+let selectionOpt = document.getElementById("selectOpt");
+
+//init selectionOpt by restoring it
+chrome.storage.sync.get('selected', function(data) {
+	selectOpt.value=data.selected;
+});
 
 //define what to do when clicking the buttons
-goButton.onclick = execFile('util/boxSelect/enableSelection.js');
+goButton.onclick = function(){
+
+	execFile('util/boxSelect/enableSelection.js');
+};
+
+//update selected in storage when opt defined
+selectOpt.onchange = function(){
+	chrome.storage.sync.set({selected: selectOpt.value}, function() {
+  		//do nothing
+ 	});
+};
--- firstAddon/util/boxSelect/enableSelection.js
+++ firstAddon/util/boxSelect/enableSelection.js
@@ -1,51 +1,122 @@
+//following code is run in scope of the web page!
 'use strict';
 console.log("Enabling Selection........");
 
 //init for controlling what is click and what is not
-let indicolor = "2px solid rgb(55, 255, 40)";
-let emptycolor = "2px solid rgb(255, 255, 255)";
+let attriColor = "2px solid rgb(0, 0, 255)"; //selected or desired border color
+let mouseoverColor = "2px solid rgb(255, 0, 0)"; //color when mouse over it
+//click controller which keeps record of the last clicking time lastT
 var clickCtr = {
 	lastT : 0 ,
-	push : function(T,e,c){
-		//by same clicks
-		if (T <= this.lastT + 12) {
-			// do nothing as the first parameter with same T booms!
-		}
-		//not same clicks
-		else {
-			e.style.border = c;
-			console.log(e.title);
-			this.lastT = T;
+	update : function(clickedElement,colorToBe){
+        //current Time
+        let currT = Math.floor(Number(new Date()));
+		//if two clicks are more than 12 ms away, they are 2 distinct clicks
+		if (currT > this.lastT + 12) {
+			clickedElement.style.border = colorToBe;
+			this.lastT = currT;
 		}
 	}
 };
 
-let dispBorder = function(){
-    event.preventDefault();
-	if(event.target.style.border === ""){
-        event.target.style.border = "2px solid rgb(255, 0, 0)";
-    }
+//return a dom element div with such color and opacity of -3
+//it is also transparent to mouse pointer!!!
+function makeMask(colorToBe){
+    var toRet = document.createElement("DIV");
+    toRet.style.background = colorToBe;
+    toRet.style.zIndex = 85;
+    toRet.style.position = "absolute";
+    toRet.style.top = "0px";
+    toRet.style.left = "0px";
+    toRet.style.width = "100%";
+    toRet.style.height = "100%";
+    toRet.style.opacity = 0.3;
+    //important!!! mask should ignore mouse!!
+    toRet.style.pointerEvents = "none";
+    //easier to check whether this element is a mask
+    toRet.maskMark = "maskMark";
+    return toRet;
 }
 
-let hideBorder = function (){    
-    event.preventDefault();
-    if(event.target.style.border === "2px solid rgb(255, 0, 0)"){
-        event.target.style.border = "";
+//boolean function indicating whether an dom's former neighbor (if it has)
+//is a mask with such color
+function hasMask(childDom, colorToMatch){
+    let prt = childDom.parentElement;
+    if(prt === null){
+        return false;
     }
+
+    let childDomIdx = Array.from(prt.childNodes).indexOf(childDom);
+    if(childDomIdx == 0){
+        //he is the eldest child!
+        return false;
+    }
+    return isMask(prt.childNodes[childDomIdx - 1], colorToMatch);
 }
 
+//check whether an dom element is a mask with certain color
+function isMask(dom, colorToMatch){
+    //is it a mask?
+    if(!dom.maskMark) return false;
+    //if so, is it such color?
+    return dom.style.background === colorToMatch;
+}
+
+//create mask silbling with indicated color of such dom
+function putOnMask(childDom, colorToBe){
+    let prt = childDom.parentElement;
+    if(prt === null){
+        return;
+    }
+    let newMask = makeMask(colorToBe);
+    prt.insertBefore(newMask, childDom);
+}
+
+//two types of transparent maskes are used: red and blue
+
+
+//when mouse over it, make sure it is masked
+let dispMask = function(){
+    event.preventDefault();
+    //if it has a red mask, ignore
+    if(hasMask(event.target, "red")) {
+        return;
+    }
+
+    //if it has a blue mask, ignore
+    if(hasMask(event.target, "blue")) {
+        return;
+    }
+
+    //no mask? put on the mask!
+    putOnMask(event.target, "red");
+
+	// if(event.target.style.border === ""){
+ //        event.target.style.border = mouseoverColor;
+ //    }
+};
+
+//when mouse out it, remove mask
+let hideMask = function (){    
+    event.preventDefault();
+    
+    // if(event.target.style.border === mouseoverColor){
+    //     event.target.style.border = "";
+    // }
+};
+
+//when clicking, update the click controller
 let clickBorder = function(e){
+    var targetElement = e.target || e.srcElement;
     e.preventDefault();
-    if (this.style.border !== attriColor) {
-        currT = Math.floor(Number(new Date()));
-        clickCtr.push(currT,this,attriColor);
+    if (targetElement.style.border !== attriColor) {
+        clickCtr.update(targetElement,attriColor);
     } else {
-        currT = Math.floor(Number(new Date()));
-        clickCtr.push(currT,this,"");
+        clickCtr.update(targetElement,"");
     }
-}
+};
 
 
-document.addEventListener("mouseover", dispBorder);
+document.addEventListener("mouseover", dispMask);
 document.addEventListener("click", clickBorder);
-document.addEventListener("mouseout", hideBorder);
+document.addEventListener("mouseout", hideMask);
